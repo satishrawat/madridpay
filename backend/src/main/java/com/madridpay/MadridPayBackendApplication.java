7ecb507d5db55f4ace207790f9150567f45b9da9
satishrawat <39967722+satishrawat@users.noreply.github.com>
2024-12-06 09:44:30 +0100

    Create MadridPayBackendApplication.java

---
+++ backend/src/main/java/com/madridpay/MadridPayBackendApplication.java
@@ -0,0 +1,11 @@
+package com.madridpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadridPayBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MadridPayBackendApplication.class, args);
+    }
+}
