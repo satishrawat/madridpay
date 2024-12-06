93145dbb9685fa0a27d1c3bd0408ba11da55a03f
satishrawat <39967722+satishrawat@users.noreply.github.com>
2024-12-06 09:59:59 +0100

    Update MadridPayBackendApplication.java

--- backend/src/main/java/com/madridpay/MadridPayBackendApplication.java
+++ backend/src/main/java/com/madridpay/MadridPayBackendApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class MadridPayBackendApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MadridPayBackendApplication.class);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(MadridPayBackendApplication.class, args);
     }
